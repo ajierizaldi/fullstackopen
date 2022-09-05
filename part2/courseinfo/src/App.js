c4f1edcc6f33fc428c5915372c3b018f775c911a
ajierzaldi7 <ajiemuhammad2@gmail.com>
2022-09-05 19:43:07 +0700

    update courseinfo in part2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,6 +1,47 @@
-import Course from './components/Course'
+// import Course from './components/Course'
 
-const App = () => {
+const Header = ({ course }) => {
+  return <h2>{course.name}</h2>
+}
+
+const Total = ({ course }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {course.parts.reduce((s, p) => s + p.exercises, 0)}
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const App = (props) => {
   const course = {
     id: 1,
     name: 'Half Stack application development',
@@ -22,7 +63,12 @@ const App = () => {
       }
     ]
   }
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <Course course={course} />
+    </div>
+  )
 }
 
 export default App;
--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,42 +1,42 @@
-const Header = ({ course }) => {
-    return <h1>{course.name}</h1>
-}
+// const Header = ({ course }) => {
+//     return <h1>{course.name}</h1>
+// }
 
-const Total = ({ course }) => {
-    return (
-        <p>
-            Number of exercises{" "}
-            {course.parts.reduce((s, p) => s + p.exercises, 0)}
-        </p>
-    )
-}
+// const Total = ({ course }) => {
+//     return (
+//         <p>
+//             Number of exercises{" "}
+//             {course.parts.reduce((s, p) => s + p.exercises, 0)}
+//         </p>
+//     )
+// }
 
-const Part = ({ part }) => {
-    return (
-        <p>
-            {part.name} {part.exercises}
-        </p>
-    )
-}
+// const Part = ({ part }) => {
+//     return (
+//         <p>
+//             {part.name} {part.exercises}
+//         </p>
+//     )
+// }
 
-const Content = ({ course }) => {
-    return (
-        <div>
-            {course.map(part => (
-                <Part key={part.id} part={part} />
-            ))}
-        </div>
-    )
-}
+// const Content = ({ course }) => {
+//     return (
+//         <div>
+//             {course.map(part => (
+//                 <Part key={part.id} part={part} />
+//             ))}
+//         </div>
+//     )
+// }
 
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content course={course.parts} />
-            <Total course={course.parts} />
-        </div>
-    )
-}
+// const Course = ({ course }) => {
+//     return (
+//         <div>
+//             <Header course={course.name} />
+//             <Content course={course.parts} />
+//             <Total course={course.parts} />
+//         </div>
+//     )
+// }
 
-export default Course
+// export default Course
